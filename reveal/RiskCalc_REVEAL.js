49e8dca2be3ce6f8aa020f414a11b971bff780e6
Blobbicus <danielk.blobbicus@gmail.com>
2025-11-02 16:09:09 +0100

    Layout change for REVEAL

--- reveal/RiskCalc_REVEAL.js
+++ reveal/RiskCalc_REVEAL.js
@@ -45,25 +45,25 @@ const HEART_RATE = {
 };
 const NYHA = {
 	name: "NYHA", group: "", meta_group: "", weight: [1, 1], value: [NaN, -1, 0, 1, 2, NaN],
-	title: "NYHA/WHO functional class <span style='color: orange'>&#9679</span>^", btnText: ["", "I", "II", "III", "IV", ""],
+	title: "NYHA/WHO functional class <span style='color: orange'>&#9679</span>", btnText: ["", "I", "II", "III", "IV", ""],
 	title_c: "", btnText_c: ["", "", "", "", "", ""],
 	title_p: "", btnText_p: ["", "", "", "", "", ""]
 };
 const WALK = {
 	name: "Walk", group: "", meta_group: "", weight: [1, 1], value: [-2, -1, 0, 1, NaN, NaN],
-	title: "Six-minute walking distance (m) <span style='color: orange'>&#9679</span>^", btnText: ["&GreaterEqual; 440", " 439 - 320", "319 - 165", "< 165", "", ""],
+	title: "Six-minute walking distance (m) <span style='color: orange'>&#9679</span>", btnText: ["&GreaterEqual; 440", " 439 - 320", "319 - 165", "< 165", "", ""],
 	title_c: "", btnText_c: ["", "", "", "", "", ""],
 	title_p: "", btnText_p: ["", "", "", "", "", ""]
 };
 const proBNP = {
 	name: "proBNP", group: "", meta_group: "", weight: [1, 1], value: [-2, NaN, 0, NaN, 2, NaN],
-	title: "NT-proBNP (ng/L)  <span style='color: orange'>&#9679</span>^#", btnText: ["< 300", "", "300 - 1099", "", "&GreaterEqual; 1100", ""],
+	title: "NT-proBNP (ng/L)  <span style='color: orange'>&#9679</span>#", btnText: ["< 300", "", "300 - 1099", "", "&GreaterEqual; 1100", ""],
 	title_c: "", btnText_c: ["", "", "", "", "", ""],
 	title_p: "", btnText_p: ["", "", "", "", "", ""]
 };
 const BNP = {
 	name: "BNP", group: "", meta_group: "", weight: [1, 1], value: [-2, NaN, 0, 1, 2, NaN],
-	title: "BNP (ng/L)  <span style='color: orange'>&#9679</span>^#", btnText: ["< 50", "", "50 - 199", "200 - 799", "&GreaterEqual; 800", ""],
+	title: "BNP (ng/L)  <span style='color: orange'>&#9679</span>#", btnText: ["< 50", "", "50 - 199", "200 - 799", "&GreaterEqual; 800", ""],
 	title_c: "", btnText_c: ["", "", "", "", "", ""],
 	title_p: "", btnText_p: ["", "", "", "", "", ""]
 };
